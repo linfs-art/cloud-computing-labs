abfc6a3715350bcce523a04977175874cd8a2e46
linfs-art <linfs_08@163.com>
2020-08-27 17:43:01 +0800

    init repo

---
+++ Lab3/src/raft_log.c
@@ -0,0 +1,141 @@
+/**
+ * Copyright (c) 2013, Willem-Hendrik Thiart
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ *
+ * @file
+ * @brief ADT for managing Raft log entries (aka entries)
+ * @author Willem Thiart himself@willemthiart.com
+ */
+
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <assert.h>
+
+#include "raft.h"
+#include "raft_private.h"
+#include "raft_log.h"
+
+#define INITIAL_CAPACITY 10
+
+typedef struct
+{
+    /* size of array */
+    raft_index_t size;
+
+    /* the amount of elements in the array */
+    raft_index_t count;
+
+    /* position of the queue */
+    raft_index_t front, back;
+
+    /* we compact the log, and thus need to increment the Base Log Index */
+    raft_index_t base;
+
+    raft_entry_t* entries;
+
+    /* callbacks */
+    raft_cbs_t *cb;
+    void* raft;
+} log_private_t;
+
+static int mod(raft_index_t a, raft_index_t b)
+{
+    int r = a % b;
+    return r < 0 ? r + b : r;
+}
+
+static int __ensurecapacity(log_private_t * me)
+{
+    raft_index_t i, j;
+    raft_entry_t *temp;
+
+    if (me->count < me->size)
+        return 0;
+
+    temp = (raft_entry_t*)__raft_calloc(1, sizeof(raft_entry_t) * me->size * 2);
+    if (!temp)
+        return RAFT_ERR_NOMEM;
+
+    for (i = 0, j = me->front; i < me->count; i++, j++)
+    {
+        if (j == me->size)
+            j = 0;
+        memcpy(&temp[i], &me->entries[j], sizeof(raft_entry_t));
+    }
+
+    /* clean up old entries */
+    __raft_free(me->entries);
+
+    me->size *= 2;
+    me->entries = temp;
+    me->front = 0;
+    me->back = me->count;
+    return 0;
+}
+
+int log_load_from_snapshot(log_t *me_, raft_index_t idx, raft_term_t term)
+{
+    log_private_t* me = (log_private_t*)me_;
+
+    log_clear_entries(me_);
+    log_clear(me_);
+    me->base = idx;
+
+    return 0;
+}
+
+int log_poll(log_t * me_, void** etyp)
+{
+    log_private_t* me = (log_private_t*)me_;
+    raft_index_t idx = me->base + 1;
+
+    if (0 == me->count)
+        return -1;
+
+    const void *elem = &me->entries[me->front];
+    if (me->cb && me->cb->log_poll)
+    {
+        int e = me->cb->log_poll(me->raft, raft_get_udata(me->raft),
+                                 &me->entries[me->front], idx);
+        if (0 != e)
+            return e;
+    }
+    me->front++;
+    me->front = me->front % me->size;
+    me->count--;
+    me->base++;
+
+    *etyp = (void*)elem;
+    return 0;
+}
+
+raft_entry_t *log_peektail(log_t * me_)
+{
+    log_private_t* me = (log_private_t*)me_;
+
+    if (0 == me->count)
+        return NULL;
+
+    if (0 == me->back)
+        return &me->entries[me->size - 1];
+    else
+        return &me->entries[me->back - 1];
+}
+
+void log_empty(log_t * me_)
+{
+    log_private_t* me = (log_private_t*)me_;
+
+    me->front = 0;
+    me->back = 0;
+    me->count = 0;
+}
+
+void log_free(log_t * me_)
+{
+    log_private_t* me = (log_private_t*)me_;
+    
+    __raft_free(me);
+}
---
+++ Lab3/src/raft_node.c
@@ -0,0 +1,84 @@
+/**
+ * Copyright (c) 2013, Willem-Hendrik Thiart
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ *
+ * @file
+ * @brief Representation of a peer
+ * @author Willem Thiart himself@willemthiart.com
+ * @version 0.1
+ */
+
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <assert.h>
+
+#include "raft.h"
+#include "raft_private.h"
+
+#define RAFT_NODE_VOTED_FOR_ME        (1 << 0)
+#define RAFT_NODE_VOTING              (1 << 1)
+#define RAFT_NODE_HAS_SUFFICIENT_LOG  (1 << 2)
+#define RAFT_NODE_INACTIVE            (1 << 3)
+#define RAFT_NODE_VOTING_COMMITTED    (1 << 4)
+#define RAFT_NODE_ADDITION_COMMITTED  (1 << 5)
+
+typedef struct
+{
+    void* udata;
+
+    raft_index_t next_idx;
+    raft_index_t match_idx;
+
+    int flags;
+
+    raft_node_id_t id;
+} raft_node_private_t;
+
+raft_node_t* raft_node_new(void* udata, raft_node_id_t id)
+{
+    raft_node_private_t* me;
+    me = (raft_node_private_t*)__raft_calloc(1, sizeof(raft_node_private_t));
+    if (!me)
+        return NULL;
+    me->udata = udata;
+    me->next_idx = 1;
+    me->match_idx = 0;
+    me->id = id;
+    me->flags = RAFT_NODE_VOTING;
+    return (raft_node_t*)me;
+}
+
+void raft_node_free(raft_node_t* me_)
+{
+    __raft_free(me_);
+}
+
+
+void raft_node_set_active(raft_node_t* me_, int active)
+{
+    raft_node_private_t* me = (raft_node_private_t*)me_;
+    if (!active)
+        me->flags |= RAFT_NODE_INACTIVE;
+    else
+        me->flags &= ~RAFT_NODE_INACTIVE;
+}
+
+void raft_node_set_voting_committed(raft_node_t* me_, int voting)
+{
+    raft_node_private_t* me = (raft_node_private_t*)me_;
+    if (voting)
+        me->flags |= RAFT_NODE_VOTING_COMMITTED;
+    else
+        me->flags &= ~RAFT_NODE_VOTING_COMMITTED;
+}
+
+int raft_node_is_active(raft_node_t* me_)
+{
+    raft_node_private_t* me = (raft_node_private_t*)me_;
+    return (me->flags & RAFT_NODE_INACTIVE) == 0;
+}
+
+
+
